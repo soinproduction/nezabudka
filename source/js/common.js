98347c61c2adb3360dfe81738de7042da32aaa51
Andrey <soinproduction@gmail.com>
2021-03-23 12:51:23 +0200

    адаптив главной

--- source/js/common.js
+++ source/js/common.js
@@ -23,7 +23,6 @@
         settings: {
           slidesToShow: 1,
           arrows: false,
-          dots: true,
           slidesToScroll: 1
         }
       },
@@ -31,9 +30,7 @@
         breakpoint: 767,
         settings: {
           slidesToShow: 1,
-          arrows: false,
           slidesToScroll: 1,
-          dots: true,
         }
       },
       {
@@ -41,14 +38,56 @@
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          dots: true,
-          arrows: false,
-          centerMode: true,
-          centerPadding: '0px',
         }
       }
     ]
   });
+
+  $('.second-slider').slick({
+    accessibility: true,
+    arrows: true,
+    dots: false,
+    slidesToShow:1,
+    speed: 1500,
+    dotsClass: "delivery-slider__dots",
+    prevArrow: '<button class="second-slider-left second-slider__button"><img src="img/arrow-left-hover.svg" alt="Слайд"></button>',
+    nextArrow: '<button class="second-slider-right second-slider__button"><img src="img/arrow-right-hover.svg" alt="Слайд"></button>',
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          arrows: true,
+          dots: false,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+
 
   $('.blog-slider').slick({
     accessibility: true,
@@ -65,7 +104,8 @@
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          arrows: true,
+          arrows: false,
+          variableWidth: true,
           dots: false,
         }
       },
@@ -74,7 +114,8 @@
         settings: {
           slidesToShow: 1,
           arrows: false,
-          dots: true,
+          dots: false,
+          variableWidth: true,
           slidesToScroll: 1
         }
       },
@@ -82,9 +123,10 @@
         breakpoint: 767,
         settings: {
           slidesToShow: 1,
-          arrows: false,
           slidesToScroll: 1,
-          dots: true,
+          arrows: false,
+          centerMode:false,
+          variableWidth: true,
         }
       },
       {
@@ -92,13 +134,20 @@
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          dots: true,
           arrows: false,
-          centerMode: true,
-          centerPadding: '0px',
+          centerMode:false,
+          variableWidth: true,
         }
       }
     ]
+  });
+
+  var $status1 = $('.mobile-count2');
+  var $slickElement1 = $('.second-slider');
+
+  $slickElement1.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+      var i = (currentSlide ? currentSlide : 0) + 1;
+      $status1.text(i + '/' + slick.slideCount);
   });
 
   var $status = $('.count');
@@ -127,64 +176,6 @@
       }
     });
   };
-
-  $(document).ready(function() {
-    $(".open-image").fancybox();
-  });
-
-  $('.tab').on('click', function(e){
-    e.preventDefault();
-    $('.tab-section').removeClass('tabs-content--active');
-    $('.tab').removeClass('tab_active');
-
-    $($(this).attr('href')).addClass('tabs-content--active');
-    $(this).addClass('tab_active');
-  });
-
-
-  // переменная не переназначается, поэтому используем const
-  // используем querySelectorAll, чтобы собрать массив со всеми сущностями .select
-  const select = document.querySelectorAll('.select');
-
-  // если массив не пустой, пробегаемся в цикле по каждой найденой сущности
-  if (select.length) {
-    select.forEach(item => {
-      // достаем из текущей сущности .select__current
-      const selectCurrent = item.querySelector('.select__current');
-
-      item.addEventListener('click', event => {
-        const el = event.target.dataset.choice;
-        const text = event.target.innerText;
-
-        // Проверяем является ли это choosen и не выбрано ли его значение уже
-        if (el === 'choosen' && selectCurrent.innerText !== text) {
-          selectCurrent.innerText = text;
-        }
-
-        item.classList.toggle('is-active');
-      });
-    });
-  }
-
-  $('.block-count__plus').click(function () {
-    var max_qty = Number($(this).parent().find('.block-count__input').attr('max'));
-    var qty = Number($(this).parent().find('.block-count__input').val());
-    if (qty > max_qty) {
-      qty = max_qty;
-    } else if (qty == max_qty) {
-      qty = max_qty;
-    } else {
-      qty += 1;
-    }
-    $(this).parent().find('.block-count__input').val(qty);
-  });
-
-  $('.block-count__minus').click(function () {
-    if ($(this).parent().find('.block-count__input').val() > 1) {
-      var plus = Number($(this).parent().find('.block-count__input').val()) - 1;
-      $(this).parent().find('.block-count__input').val(plus);
-    }
-  });
 
   $(".hamburger").click(function(event) {
     $(".hamburger").toggleClass('hamburger__active'),
